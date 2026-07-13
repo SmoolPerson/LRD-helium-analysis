e68c7ad7d97a3dc05f1679c2633fefb4a3b9a5fb
SmoolPerson <mincymint@proton.me>
2026-07-13 14:13:21 -0500

    tweak parameters in pyneb analysis and calculate flux ratio error

--- calculate_ratios.py
+++ calculate_ratios.py
@@ -5,7 +5,7 @@ df = pd.read_csv('flux/fluxes.csv')
 
 df = df.sort_values(by='Observation')
 
-ratios_dict = {"Observation": [], "Flux Ratio": []}
+ratios_dict = {"Observation": [], "Flux Ratio": [], "Flux Ratio Error": []}
 
 for i in range(len(df)):
     current_row = df.iloc[i]
@@ -13,8 +13,12 @@ for i in range(len(df)):
     if np.isnan(current_row.loc["Profile Flux-He1_7065A"])  or np.isnan(current_row.loc["Profile Flux-He1_5876A"]):
         continue
     ratio = current_row.loc["Profile Flux-He1_5876A"]/current_row.loc["Profile Flux-He1_7065A"]
+
+    relative_error1 = current_row.loc["Profile Flux Error-He1_5876A"]/current_row.loc["Profile Flux-He1_5876A"]
+    relative_error2 = current_row.loc["Profile Flux Error-He1_7065A"]/current_row.loc["Profile Flux-He1_7065A"]
     ratios_dict["Observation"].append(current_row.loc["Observation"])
     ratios_dict["Flux Ratio"].append(ratio)
+    ratios_dict["Flux Ratio Error"].append(ratio * (relative_error1 + relative_error2))
 
 ratios_df = pd.DataFrame(ratios_dict)
 ratios_df.to_csv('flux/flux_ratios.csv', index=False)
--- pyneb_analysis.py
+++ pyneb_analysis.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 STEPS = 50
 
-START_TEMP = 7600
+START_TEMP = 5500
 STOP_TEMP = 25000
-START_DEN = 1e4
+START_DEN = 1e12
 STOP_DEN = 1e13
 
 he1 = pn.RecAtom('He', 1)
@@ -35,41 +35,42 @@ for j in range(STEPS):
         matrix[j, i] = he1_5876/he1_7065
         for k in range(len(df)):
             row = df.iloc[k]
-            diff = abs(float(row.loc["Flux Ratio"]) - matrix[i][j])
+            diff = abs(float(row.loc["Flux Ratio"]) - matrix[j, i])
             if diff < closest_ratios[k]:
                 closest_ratios[k] = diff
-                x_coords[k] = ((i + 0.5) / STEPS)
-                y_coords[k] = (((STEPS - j - 1) - 0.5) / STEPS)
+                x_coords[k] = ((i + 0.5) / STEPS) * (STOP_TEMP - START_TEMP) + START_TEMP
+                y_coords[k] = (((STEPS - j - 1) - 0.5) / STEPS) * (STOP_DEN - START_DEN) + START_DEN
 
 ax = plt.gca()
-plt.imshow(matrix, extent=[START_TEMP, STOP_TEMP, START_DEN,STOP_DEN], aspect="auto", interpolation="none")
-plt.colorbar(label = "Emissivity Ratio")
-#for i in range(len(labels)): # not working idk why
-    #plt.annotate(labels[i], (x_coords[i], y_coords[i]), xytext=(0.1, 0.9), textcoords='axes points', color="orange", transform=ax.transAxes)
+print(x_coords, y_coords)
+plt.imshow(matrix, extent=[START_TEMP, STOP_TEMP, START_DEN,STOP_DEN], aspect="auto")
+plt.colorbar(label = "Flux Ratio")
+for i in range(len(labels)): # not working idk why
+    plt.annotate(labels[i], (x_coords[i], y_coords[i]), textcoords='data', color="orange", fontsize=7)
 plt.xlabel("Temperature (K)")
 plt.ylabel("Density ($cm^{-3}$)")
-plt.scatter(x_coords, y_coords, color='red', marker='x', transform=ax.transAxes)
+plt.scatter(x_coords, y_coords, color='red', marker='x')
 plt.savefig("pyneb_plots/emissivity_matrix.png")
 plt.clf()
 
 # final plot
-dot_temperatures = []
-for coord in x_coords:
-    dot_temperatures.append(coord * (STOP_TEMP - START_TEMP) + START_TEMP)
-
-print(dot_temperatures)
 print(list(df.loc[:, "Flux Ratio"]))
+print(closest_ratios)
 
 # plot lines for each density
 for i in range(0, STEPS, 10):
     den_val = (i/STEPS) * (STOP_DEN - START_DEN) + START_DEN
     line_plot = []
     for j in range(STEPS):
-        line_plot.append(matrix[j, i])
+        line_plot.append(matrix[i, j])
     plt.plot(temperatures, line_plot, linestyle='--', label=f"Density: {den_val:.2e}")
 
 plt.xlabel("Temperature (K)")
-plt.ylabel("Emissivity ratio")
+plt.ylabel("Flux ratio")
 plt.legend()
-plt.scatter(dot_temperatures, list(df.loc[:, "Flux Ratio"]), color='red', marker='x')
+for i in range(len(labels)): # not working idk why
+    x_coord = x_coords[i]
+    y_coord = list(df.loc[:, "Flux Ratio"])[i]
+    plt.annotate(labels[i], (x_coord, y_coord), textcoords='offset points', xytext=(5, 5), color="orange", fontsize=7, xycoords="data")
+plt.scatter(x_coords, list(df.loc[:, "Flux Ratio"]), color='red', marker='x')
 plt.savefig("pyneb_plots/final_plot.png")
